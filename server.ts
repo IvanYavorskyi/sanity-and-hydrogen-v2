67ee36cd5d1a9f6da84c60d5cd045e427c9f5f19
Ivan Yavorskyi <ivan.yavorskyi@digitalsuits.co>
2025-06-25 16:15:57 +0000

    update configs

--- server.ts
+++ server.ts
@@ -1,119 +1,253 @@
+// Virtual entry point for the app
 import * as remixBuild from '@remix-run/dev/server-build';
-// import * as remixBuild from '~/dist/server/index.js';
-import {createRequestHandler} from '@remix-run/server-runtime';
-
 import {
   cartGetIdDefault,
   cartSetIdDefault,
   createCartHandler,
   createStorefrontClient,
-  createCustomerAccountClient,
+  storefrontRedirect,
 } from '@shopify/hydrogen';
-
-import {AppSession} from '~/lib/session';
-import {CART_QUERY_FRAGMENT} from '~/lib/fragments';
+import {
+  createRequestHandler,
+  getStorefrontHeaders,
+  createCookieSessionStorage,
+  type SessionStorage,
+  type Session,
+} from '@shopify/remix-oxygen';
 
 /**
  * Export a fetch handler in module format.
  */
-
-export default async function (request) {
-  try {
-    /**
-     * This has to be done so messy because process.env can't be destructured
-     * and only variables explicitly named are present inside a Vercel Edge Function.
-     * See https://github.com/vercel/next.js/pull/31237/files
-     */
-    const env = {
-      SESSION_SECRET: '',
-      PUBLIC_STOREFRONT_API_TOKEN: '',
-      PRIVATE_STOREFRONT_API_TOKEN: '',
-      PUBLIC_STORE_DOMAIN: '',
-      PUBLIC_CUSTOMER_ACCOUNT_API_CLIENT_ID: '',
-      PUBLIC_CUSTOMER_ACCOUNT_API_URL: '',
-      PUBLIC_CHECKOUT_DOMAIN: '',
-    };
-
-    env.SESSION_SECRET = process.env.SESSION_SECRET;
-    env.PUBLIC_STOREFRONT_API_TOKEN = process.env.PUBLIC_STOREFRONT_API_TOKEN;
-    env.PRIVATE_STOREFRONT_API_TOKEN = process.env.PRIVATE_STOREFRONT_API_TOKEN;
-    env.PUBLIC_STORE_DOMAIN = process.env.PUBLIC_STORE_DOMAIN;
-    env.PUBLIC_CUSTOMER_ACCOUNT_API_CLIENT_ID = process.env.PUBLIC_CUSTOMER_ACCOUNT_API_CLIENT_ID;
-    env.PUBLIC_CUSTOMER_ACCOUNT_API_URL = process.env.PUBLIC_CUSTOMER_ACCOUNT_API_URL;
-    env.PUBLIC_CHECKOUT_DOMAIN = process.env.PUBLIC_CHECKOUT_DOMAIN;
-
-    /**
-     * Open a cache instance in the worker and a custom session instance.
-     */
-    if (!env?.SESSION_SECRET) {
-      throw new Error('SESSION_SECRET process.environment variable is not set');
-    }
-
-    const [session] = await Promise.all([
-      AppSession.init(request, [process.env.SESSION_SECRET]),
-    ]);
-
-    /**
-     * Create Hydrogen's Storefront client.
-     */
-    const {storefront} = createStorefrontClient({
-      i18n: {language: 'EN', country: 'US'},
-      publicStorefrontToken: env.PUBLIC_STOREFRONT_API_TOKEN,
-      privateStorefrontToken: env.PRIVATE_STOREFRONT_API_TOKEN,
-      storeDomain: env.PUBLIC_STORE_DOMAIN,
-      // storefrontId: process.env.PUBLIC_STOREFRONT_ID,
-      // requestGroupId: request.headers.get('request-id'),
-    });
-
-    /**
-     * Create a client for Customer Account API.
-     */
-    const customerAccount = createCustomerAccountClient({
-      request,
-      session,
-      customerAccountId: env.PUBLIC_CUSTOMER_ACCOUNT_API_CLIENT_ID,
-      customerAccountUrl: env.PUBLIC_CUSTOMER_ACCOUNT_API_URL,
-    });
-
-    const cart = createCartHandler({
-      storefront,
-      customerAccount,
-      getCartId: cartGetIdDefault(request.headers),
-      setCartId: cartSetIdDefault(),
-      cartQueryFragment: CART_QUERY_FRAGMENT,
-    });
-
-    const handleRequest = createRequestHandler(remixBuild, 'production');
-
-    const response = await handleRequest(request, {
-      session,
-      storefront,
-      customerAccount,
-      cart,
-      env,
-      waitUntil: () => Promise.resolve(),
-    });
-
-    if (session.isPending) {
-      response.headers.set(
-        'Set-Cookie',
-        await session.commit(),
-      );
-    }
-
-    if (response.status === 404) {
+export default {
+  async fetch(
+    request: Request,
+    env: Env,
+    executionContext: ExecutionContext,
+  ): Promise<Response> {
+    try {
       /**
-       * Check for redirects only when there's a 404 from the app.
-       * If the redirect doesn't exist, then `storefrontRedirect`
-       * will pass through the 404 response.
+       * Open a cache instance in the worker and a custom session instance.
        */
-      // return storefrontRedirect({request, response, storefront});
-    }
+      if (!env?.SESSION_SECRET) {
+        throw new Error('SESSION_SECRET environment variable is not set');
+      }
 
-    return response;
-  } catch (error) {
-    // eslint-disable-next-line no-console
-    console.error(error);
-    return new Response('An unexpected error occurred', {status: 500});
+      const waitUntil = (p: Promise<any>) => executionContext.waitUntil(p);
+      const [cache, session] = await Promise.all([
+        caches.open('hydrogen'),
+        HydrogenSession.init(request, [env.SESSION_SECRET]),
+      ]);
+
+      /**
+       * Create Hydrogen's Storefront client.
+       */
+      const {storefront} = createStorefrontClient({
+        cache,
+        waitUntil,
+        i18n: {language: 'EN', country: 'US'},
+        publicStorefrontToken: env.PUBLIC_STOREFRONT_API_TOKEN,
+        privateStorefrontToken: env.PRIVATE_STOREFRONT_API_TOKEN,
+        storeDomain: env.PUBLIC_STORE_DOMAIN,
+        storefrontId: env.PUBLIC_STOREFRONT_ID,
+        storefrontHeaders: getStorefrontHeaders(request),
+      });
+
+      /*
+       * Create a cart handler that will be used to
+       * create and update the cart in the session.
+       */
+      const cart = createCartHandler({
+        storefront,
+        getCartId: cartGetIdDefault(request.headers),
+        setCartId: cartSetIdDefault(),
+        cartQueryFragment: CART_QUERY_FRAGMENT,
+      });
+
+      /**
+       * Create a Remix request handler and pass
+       * Hydrogen's Storefront client to the loader context.
+       */
+      const handleRequest = createRequestHandler({
+        build: remixBuild,
+        mode: process.env.NODE_ENV,
+        getLoadContext: () => ({session, storefront, env, cart}),
+      });
+
+      const response = await handleRequest(request);
+
+      if (response.status === 404) {
+        /**
+         * Check for redirects only when there's a 404 from the app.
+         * If the redirect doesn't exist, then `storefrontRedirect`
+         * will pass through the 404 response.
+         */
+        return storefrontRedirect({request, response, storefront});
+      }
+
+      return response;
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.error(error);
+      return new Response('An unexpected error occurred', {status: 500});
+    }
+  },
+};
+
+/**
+ * This is a custom session implementation for your Hydrogen shop.
+ * Feel free to customize it to your needs, add helper methods, or
+ * swap out the cookie-based implementation with something else!
+ */
+export class HydrogenSession {
+  constructor(
+    private sessionStorage: SessionStorage,
+    private session: Session,
+  ) {}
+
+  static async init(request: Request, secrets: string[]) {
+    const storage = createCookieSessionStorage({
+      cookie: {
+        name: 'session',
+        httpOnly: true,
+        path: '/',
+        sameSite: 'lax',
+        secrets,
+      },
+    });
+
+    const session = await storage.getSession(request.headers.get('Cookie'));
+
+    return new this(storage, session);
+  }
+
+  has(key: string) {
+    return this.session.has(key);
+  }
+
+  get(key: string) {
+    return this.session.get(key);
+  }
+
+  destroy() {
+    return this.sessionStorage.destroySession(this.session);
+  }
+
+  flash(key: string, value: any) {
+    this.session.flash(key, value);
+  }
+
+  unset(key: string) {
+    this.session.unset(key);
+  }
+
+  set(key: string, value: any) {
+    this.session.set(key, value);
+  }
+
+  commit() {
+    return this.sessionStorage.commitSession(this.session);
   }
 }
+
+// NOTE: https://shopify.dev/docs/api/storefront/latest/queries/cart
+const CART_QUERY_FRAGMENT = `#graphql
+  fragment Money on MoneyV2 {
+    currencyCode
+    amount
+  }
+  fragment CartLine on CartLine {
+    id
+    quantity
+    attributes {
+      key
+      value
+    }
+    cost {
+      totalAmount {
+        ...Money
+      }
+      amountPerQuantity {
+        ...Money
+      }
+      compareAtAmountPerQuantity {
+        ...Money
+      }
+    }
+    merchandise {
+      ... on ProductVariant {
+        id
+        availableForSale
+        compareAtPrice {
+          ...Money
+        }
+        price {
+          ...Money
+        }
+        requiresShipping
+        title
+        image {
+          id
+          url
+          altText
+          width
+          height
+
+        }
+        product {
+          handle
+          title
+          id
+        }
+        selectedOptions {
+          name
+          value
+        }
+      }
+    }
+  }
+  fragment CartApiQuery on Cart {
+    id
+    checkoutUrl
+    totalQuantity
+    buyerIdentity {
+      countryCode
+      customer {
+        id
+        email
+        firstName
+        lastName
+        displayName
+      }
+      email
+      phone
+    }
+    lines(first: $numCartLines) {
+      nodes {
+        ...CartLine
+      }
+    }
+    cost {
+      subtotalAmount {
+        ...Money
+      }
+      totalAmount {
+        ...Money
+      }
+      totalDutyAmount {
+        ...Money
+      }
+      totalTaxAmount {
+        ...Money
+      }
+    }
+    note
+    attributes {
+      key
+      value
+    }
+    discountCodes {
+      code
+      applicable
+    }
+  }
+` as const;
