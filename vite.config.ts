1a833f31df411b8a20cea9fe6b23b06c793e739c
Ivan Yavorskyi <ivan.yavorskyi@digitalsuits.co>
2025-06-25 16:52:52 +0000

    update

--- vite.config.ts
+++ vite.config.ts
@@ -6,20 +6,20 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 export default defineConfig({
   plugins: [
     hydrogen(),
-    remix({
-      // ← use v3preset(), not preset()
-      presets: [hydrogen.v3preset()],
-      // your future flags here (opt-in to React Router 7 behaviors)
-      future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-        v3_lazyRouteDiscovery: true,
-        v3_routeConfig: true,
-        v3_singleFetch: true,
-      },
-      ignoredRouteFiles: ['**/.*'],
-    }),
+    // remix({
+    //   // ← use v3preset(), not preset()
+    //   presets: [hydrogen.v3preset()],
+    //   // your future flags here (opt-in to React Router 7 behaviors)
+    //   future: {
+    //     v3_fetcherPersist: true,
+    //     v3_relativeSplatPath: true,
+    //     v3_throwAbortReason: true,
+    //     v3_lazyRouteDiscovery: true,
+    //     v3_routeConfig: true,
+    //     v3_singleFetch: true,
+    //   },
+    //   ignoredRouteFiles: ['**/.*'],
+    // }),
     tsconfigPaths(),
   ],
   build: {assetsInlineLimit: 0},
