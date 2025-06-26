5cad777a1e644cf152935079a52d389857cc7f6a
Ivan Yavorskyi <ivan.yavorskyi@digitalsuits.co>
2025-06-26 10:12:47 +0000

    update configs

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,8 +1,10 @@
 // react-router.config.ts
 import type {Config} from '@react-router/dev/config';
+import { vercelPreset } from '@vercel/react-router/vite';
 
 export default {
   appDirectory: 'app',
   buildDirectory: 'dist',
   ssr: true,
+  presets: [vercelPreset()],
 } satisfies Config;
